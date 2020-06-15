eb0cf476086fc9b72669c7728ad98283ef24d1b1
陈新元 <chenxinyuan@zondycyber.com>
2020-06-15 18:38:48 +0800

    update protobuf

---
+++ protobuf/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"os"
+	"protobuf/example"
+)
+
+func main() {
+	msg_test := &example.Person{
+		Name: proto.String("cxy"),
+		Age:  proto.Int32(18),
+		From: proto.String("china"),
+	}
+
+	// 序列化
+	msgDataEncoding, err := proto.Marshal(msg_test)
+	if err != nil {
+		panic(err.Error())
+		return
+	}
+
+	msgEntity := example.Person{}
+	err = proto.Unmarshal(msgDataEncoding, &msgEntity)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+		return
+	}
+
+	fmt.Printf("name: %s\n", msgEntity.GetName())
+	fmt.Printf("name: %v\n", *msgEntity.Name)
+	fmt.Printf("age: %d\n", msgEntity.GetAge())
+	fmt.Printf("from: %s\n", msgEntity.GetFrom())
+}
